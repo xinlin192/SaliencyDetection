42fb99ff0b242858ac597317774061de04761ef0
JimmyLin192 <linxin@gmail.com>
2013-05-30 20:14:17 +1000

    refactor the repository and higher abstraction

--- testColorSpatialDistribution.sh
+++
@@ -1,4 +0,0 @@
-echo "Which training data set to use: "
-read x
-mkdir results/ColorSpatialDistribution/"$x"/
-./../../bin/testColorSpatialDistribution -verbose dataset/images/A/"$x" dataset/labels/A/"$x"_data.txt results/ColorSpatialDistribution/"$x"/
--- testMultiscaleContrast.sh
+++
@@ -1,6 +0,0 @@
-for x in $*
-do  
-    mkdir results/BMultiscaleContrast/
-    mkdir results/BMultiscaleContrast/"$x"/
-    ./../../bin/trainModel -verbose -x dataset/images/B/$x dataset/labels/B/"$x"_data.txt results/BMultiscaleContrast/"$x"/
-done
